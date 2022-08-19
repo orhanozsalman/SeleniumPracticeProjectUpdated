5c246d4b3b7d5176581f00dd10a46e50e32848c5
orhanozsalman <orhanozsalman@hotmail.com>
2022-08-19 11:22:55 +0200

    Actions

--- src/test/java/cybertek/tests/P30_ActionClassTest.java
+++ src/test/java/cybertek/tests/P30_ActionClassTest.java
@@ -1,8 +1,10 @@
 package cybertek.tests;
 
 import cybertek.test_util.TestBase;
+import io.cucumber.java.bs.A;
 import io.cucumber.java.en_old.Ac;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
@@ -69,6 +71,80 @@ public class P30_ActionClassTest  extends TestBase {
 
         Thread.sleep(3000);
 
+    }
+
+    @Test
+    public void testKeyboardActions(){
+
+
+        driver.get("https://www.google.com/");
+        WebElement searchBox = driver.findElement(By.name("q"));
+
+
+        //click on the element
+
+        // hold shift and enter text
+
+        //release the shift and enter
+
+        //select all and delete
+
+        Actions actions = new Actions(driver);
+        actions.click(searchBox).pause(1000)
+                .keyDown(Keys.SHIFT)
+                .sendKeys("typing by holding shift").pause(1000)
+                .keyUp(Keys.SHIFT)
+                .sendKeys("typing after releasing").pause(1000)
+                .keyDown(Keys.CONTROL).sendKeys("A").pause(1000)
+                .keyUp(Keys.CONTROL)
+                .sendKeys(Keys.BACK_SPACE).pause(1000)
+                .perform();
+
+
+
+
+
+
+    }
+
+
+    @Test
+    public void testRightCLick() throws InterruptedException{
+
+        driver.get("https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_oncontextmenu");
+
+        //locate the div element inside iFrame with css selector()
+        //
+        driver.switchTo().frame("iframeResult");
+        WebElement area = driver.findElement(By.cssSelector("div[contextmenu=\"mymenu\"]"));
+
+        Actions actions =  new Actions(driver);
+        actions.contextClick(area).perform();
+
+        Thread.sleep(1000);
+
+        driver.switchTo().alert().accept();
+
+        Thread.sleep(1000);
+
+
+    }
+
+    @Test
+    public void testDoubleClick(){
+
+        driver.get("https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_ondblclick");
+
+        driver.switchTo().frame("iframeResult");
+
+        WebElement pElm= driver.findElement(By.xpath("//p['Double-click this paragraph to trigger a function.']"));
+
+        Actions actions = new Actions(driver);
+        actions.doubleClick(pElm).perform();
+
+        //assert p element
+        WebElement resultELm =  driver.findElement(By.xpath("//p[.='Hello World']"));
+        assertTrue(resultELm.isDisplayed());
     }
 
 }
